c4e8b425a6c3fbdf90fc01596e200304f3e9b07f
elyonnelly <alfedorova.ru@gmail.com>
2020-12-09 03:59:39 +0500

    add solution

--- module-1/homework/List/list.h
+++ module-1/homework/List/list.h
@@ -1,51 +1,74 @@
 #pragma once
+
 #include <cstddef>
 
 
 namespace task {
+    
+    class list {
+
+    public:
+
+        list();
+
+        list(size_t count, const int &value = int());
+
+        list(const task::list &other);
+
+        ~list();
+
+        list &operator=(const list &other);
 
 
-class list {
+        int &front();
 
-public:
+        const int &front() const;
 
-    list();
-    list(size_t count, const int& value = int());
+        int &back();
 
-    ~list();
-    list& operator=(const list& other);
+        const int &back() const;
 
 
-    int& front();
-    const int& front() const;
+        bool empty() const;
 
-    int& back();
-    const int& back() const;
+        size_t size() const;
 
-
-    bool empty() const;
-    size_t size() const;
-    void clear();
+        void clear();
 
 
-    void push_back(const int& value);
-    void pop_back();
-    void push_front(const int& value);
-    void pop_front();
-    void resize(size_t count);
-    void swap(list& other);
+        void push_back(const int &value);
 
+        void pop_back();
 
-    void remove(const int& value);
-    void unique();
-    void sort();
+        void push_front(const int &value);
 
-    // Your code goes here?..
+        void pop_front();
 
-private:
+        void resize(size_t count);
 
-    // Your code goes here...
+        void swap(list &other);
 
-};
+        void remove(const int &value);
 
-}  // namespace task
+        void unique();
+
+        void sort();
+
+    private:
+
+        struct Node {
+            int value;
+            Node *prev = nullptr;
+            Node *next = nullptr;
+
+            explicit Node(int value_, Node *prev_, Node *next_) : value(value_), prev(prev_), next(next_) {}
+        };
+
+        Node *head;
+        Node *tail;
+        size_t list_size;
+
+        void remove_node(Node *node);
+    };
+
+}
